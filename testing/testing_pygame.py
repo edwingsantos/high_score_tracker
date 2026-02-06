bad13408e8284ea9a82bcccd8d3b423b376420d1
JS-CP1 <joseph.stratford@ucas-edu.net>
2026-02-06 09:51:03 -0700

    flappy bird update

--- testing/testing_pygame.py
+++ testing/testing_pygame.py
@@ -1,6 +1,7 @@
 def flappy_bird():
     import pygame
     import sys
+    import random
     pygame.init()
 
     class Bird(pygame.sprite.Sprite):
@@ -34,7 +35,17 @@ def flappy_bird():
     
     class Pipe(pygame.sprite.Sprite):
         def __init__(self):
-            pass
+            super().__init__()
+            original = pygame.image.load("testing/pipe.png").convert_alpha()
+            self.original_image = pygame.transform.scale(original, (50, 1000))
+            self.image = self.original_image
+            self.rect = self.image.get_rect()
+            self.rect.center = (screen_width - (screen_width // 4), screen_height // 2)
+            self.point = [screen_width, screen_height-(50+(random.randint(1,750)))]
+        
+        def update(self):
+            self.point[0] -= 5
+            self.rect.centerx -= 5
     
     screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
     clock = pygame.time.Clock()
@@ -46,6 +57,10 @@ def flappy_bird():
     bird_group = pygame.sprite.Group()
     bird_group.add(bird)
 
+    pipe = Pipe()
+    pipe_group = pygame.sprite.Group()
+    pipe_group.add(pipe)
+    
     running = True
     start = False
     while running:
@@ -58,18 +73,25 @@ def flappy_bird():
             if event.type == pygame.KEYDOWN and event.key == pygame.K_UP or event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
                 start = True
                 bird.jump()
+            if pygame.sprite.collide_rect(bird_group, pipe_group) == True:
+                running = False
 
         # SET UP
         screen.fill("light blue")
         bird_group.draw(screen)
+        pipe_group.draw(screen)
         
         # UPDATE
         if start == True:
+            # Move dot across x
+            # Update pipe
             bird_group.update()
+            pipe_group.update()
             if bird.rect.bottom >= screen_height - 10:
                 running = False
             if bird.rect.top <= -30:
                 running = False
+            
             
         pygame.display.flip()
         clock.tick(60)
