e0b4f8e56ce1ca07fa877ae1069c42b6f1d0e06c
nicole12131 <neilybeth.salom@ucas-edu.net>
2026-02-20 09:34:00 -0700

    stared the main

--- src/main.py
+++ src/main.py
@@ -1,1 +1,10 @@
-# hi
+# NS 1st
+
+#Make a function called main
+    #make a while loop
+    #Ask user if they are loged in or if they want to exit
+        #if user choice is logeg in, call the loged in funtion
+        #elif user chioce is not loged in, call the not logged in funtion
+        #elif user choice is exit, break
+        #else print select an actual option
+
