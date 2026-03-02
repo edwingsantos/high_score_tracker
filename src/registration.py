e966bb33d7921a3737bd5ca099a32ad7973128ed
edwingsantos <edwing.santos@ucas-edu.net>
2026-03-02 09:52:46 -0700

    almost finised

--- src/registration.py
+++ src/registration.py
@@ -1,53 +1,55 @@
 import csv
-<<<<<<< HEAD
-
+import login_logout
 while True:
-    temp_user = input("Please create a username:\n")
-    with open("docs/accounts.csv", mode="r", newline="") as file:
-        reader = csv.DictReader(file)
-        for i, row in enumerate(reader, 1):
-            item = {"username": row.get("username", "").strip(), "password": row.get("password", "").strip(), "flappybird": row.get("flappybird", "").strip(), "reaction time": row.get("reaction time", "").strip(), "pong": row.get("pong.strip")}
-            if temp_user == "username":
-                print("That username is not availible, would you like to log into that account?")
-                
-            
-=======
-import login_logout.py
+    #ask user to create a username
+    username = input("Please input a new username: ")
 
-#ask user to create a username
-username = input("Please input a new username: ")
+    #open accounts csv and loop through every row
+    with open("docs/accounts.csv", "r") as file:
+        csv_reader = csv.reader()
+    #    if a row has that username
+        for row in csv_reader:
+            if username in row:
+#           tell the user that there is already and account with that name
+                print("There is already an account with that name.")
+            else:
+#           ask them if they would like to try and login to that account
+                login = input("Would you like to log in with that name? (Y/N)").strip()
+#               if they would
+                if login == "Y":
+#               call the login_logout function
+                    login_logout()
+#               if they want still want to create an account
+                if login == "N":
+#               loop back to the beginning of the file (continue in python)
+                    continue
+    #ask user if they would like a generated password or want to make one themselves
+    password_choice = input("What would you like to do: \n 1. Type your own password \n 2. Generate a new password\nChoice: ")
+         #if they choose to have a password generated for them
+    if password_choice == "1":
+    #    ask the user to select the requirements they would like for their password
+        requirements = input("What requrements would you like?")
+    #    generate a password according to those requirements
+    #    ask the user if they like the password given
 
-#open accounts csv and loop through every row
-with open("docs/accounts.csv", "r") as file:
-    csv_reader = csv.reader()
-#    if a row has that username
-    for row in csv_reader:
-        if username in row:
-#        tell the user that there is already and account with that name
-            print("There is already an account with that name.")
-#        ask them if they would like to try and login to that account
-            login = input("Would you like to log in to the user with that name?")
-#        if they would
-#            call the login_logout function
-#        if they want still want to create an account
-#            loop back to the beginning of the file (continue in python)
-#ask user if they would like a generated password or want to make one themselves
-#if they choose to have a password generated for them
-#    ask the user to select the requirements they would like for their password
-#    generate a password according to those requirements
-#    ask the user if they like the password given
-#    if they don't
-#        loop back through this section (the generate password section)
-#if they choose to create a password
-#    ask the user for the password they'd like to use
-#    ask them to confirm if that's the password they'd like
-#    if it isn't
-#        loop back through this section (the create password section)
-#if their name is admin and their password is YIPPEESKIPPEE
-#    make an admin variable true
-#otherwise
-#    make the admin variable false
-#open the accounts csv
-#    insert/append a new row on the bottom with the username and password they've inputted and all their scores as 0
-#    return back to where it was called with admin
->>>>>>> 92f659f27262cc38474d105ff21e2e7a4e5b817c
+     # if they don't
+    elif password_choice == "2":
+
+    #        loop back through this section (the generate password section)
+    #if they choose to create a password
+    #    ask the user for the password they'd like to use
+    #    ask them to confirm if that's the password they'd like
+    #    if it isn't
+    #        loop back through this section (the create password section)
+    #if their name is admin and their password is YIPPEESKIPPEE
+    if username == "admin" and password == "YIPPEESKIPPEE":
+    #    make an admin variable true
+        admin = True
+    #otherwise
+    else:
+    #    make the admin variable false
+        admin = False
+    #open the accounts csv
+    with open("docs/accounts.csv", "a") as file:
+    #    insert/append a new row on the bottom with the username and password they've inputted and all their scores as 0
+    #    return back to where it was called with admin
