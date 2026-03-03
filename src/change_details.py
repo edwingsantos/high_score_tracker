f71555c2d29e2f99e9e5cf2c37a99e57e817a404
edwingsantos <edwing.santos@ucas-edu.net>
2026-03-02 21:39:29 -0700

    finished with everything i think, but i couldn't test it

--- src/change_details.py
+++ src/change_details.py
@@ -1,5 +1,5 @@
 import csv
-
+import registration
 
 #make a funtion for change detail
 def change(account):
@@ -26,7 +26,7 @@ def change(account):
             print("line doesn't exist")
             return
 #    call user registration funtion
-    
+    registration()
 
 
 
